9263e56a03d02c0ba47d33e48a60c2996a338e14
Shadow Smith <shadow@businesscreditreports.com>
2020-03-23 13:49:57 -0500

    Add pinterest color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        pinterest: '#e60023'
+      }
+    }
+  },
   variants: {},
   plugins: []
 }
