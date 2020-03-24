eba530fdea46612950545ee12c9b7f7bc472ecaf
Shadow Smith <shadow@businesscreditreports.com>
2020-03-24 08:46:31 -0500

    Update admin page

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="bg-gray-100 min-h-screen pb-6">
-    <nuxt-child />
-    <p class="text-center text-xs text-gray-500">
-      {{ new Date().getFullYear() }} PinterestCMS, All Rights Reserved.
-    </p>
+    <div v-if="loggedIn">
+      <nuxt-child />
+      <p class="text-center text-xs text-gray-500">
+        {{ new Date().getFullYear() }} PinterestCMS, All Rights Reserved.
+      </p>
+    </div>
   </div>
 </template>
 
