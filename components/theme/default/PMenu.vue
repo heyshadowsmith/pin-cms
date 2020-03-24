b94d8eaef5530415809f68540fce8d5f320b2ba7
Shadow Smith <shadow@businesscreditreports.com>
2020-03-24 16:50:37 -0500

    Add default theme components

---
+++ components/theme/default/PMenu.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="px-6 flex justify-between items-center">
+    <nuxt-link to="/" class="py-4 px-3">
+      <Logo />
+    </nuxt-link>
+    <div>
+      <nuxt-link v-for="(link, index) in links" :key="index" class="text-xs inline-block py-4 px-3 uppercase font-bold" :to="`/${link.slug}`">
+        {{ link.name }}
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/theme/default/Logo'
+
+export default {
+  name: 'PMenu',
+  components: {
+    Logo
+  },
+  props: {
+    links: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
