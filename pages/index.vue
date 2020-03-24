539b36dd16e80c191fe4dec59252a3a288cf3f17
Shadow Smith <shadow@businesscreditreports.com>
2020-03-24 13:08:40 -0500

    Debugging

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <nuxt-link to="/">
+    <!-- <nuxt-link to="/">
       Home
-    </nuxt-link>
+    </nuxt-link> -->
     <nuxt-link v-for="(menuItem, index) in menuItems" :key="index" :to="`/${menuItem.slug}`">
       {{ menuItem.name }}
     </nuxt-link>
